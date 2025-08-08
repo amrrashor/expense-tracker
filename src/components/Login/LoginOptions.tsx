9a22bfcf74d65558d9e8173169bd5abf7d8c1ec7
Amr Ashour <75447356+amrrashor@users.noreply.github.com>
2025-08-08 17:33:33 +0300

    finished login screen

---
+++ src/components/Login/LoginOptions.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Button } from '../common/ui/Button'
+const LoginOptions = () => {
+    return (
+        <div>
+            <div className='mb-5'>
+r                <Button className='border-input-bg borde text-center w-full  p-6 text-lg rounded-lg'>
+                    <img src='/src/assets/google.webp' alt='google logo' className='w-4' /> continue with Google
+                </Button>
+            </div>
+
+            <div className='mb-5'>
+                <Button className='border-input-bg border text-center w-full  p-6 text-lg rounded-lg'>
+                    <img src='/src/assets/Apple.svg' alt='apple logo' className='w-4' /> continue with Apple
+                </Button>
+            </div>
+
+            <div className='text-gray-1 text-sm text-center'>
+                Don't have an account? <a  href='' className=' underline text-primary-blue-1'>Sign Up</a>
+            </div>
+        </div>
+    )
+}
+
+export default LoginOptions
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,34 @@
 import {motion} from "motion/react"
 import { ANIMATE_TRANSITION, EXIT_TRANSITION, INITIAL_TRANSITION } from "../constants/animations"
+import LoginForm from "../components/Login/LoginForm"
+import Divider from "../components/common/ui/Divider"
+import LoginOptions from "../components/Login/LoginOptions"
+import { toast } from "sonner"
 
 const Login = () => {
+    const handleFormSubmit = (data: { email: string, password: string }) => {
+        localStorage.setItem("userInfo", JSON.stringify(data));
+        toast.success('Logged Succefully');
+        setTimeout(() => {
+            window.location.reload();
+        }, 1000)
+    };
+
     return (
             <motion.div
                 initial={INITIAL_TRANSITION}
                 animate={ANIMATE_TRANSITION}
                 exit={EXIT_TRANSITION}
                 transition={{ duration: 0.3 }}
+                className="px-5"
             >
-                Login
+                <div className="my-10">
+                    <h1 className="text-4xl text-primary-blue font-bold text-center">Expenset.</h1>
+                </div>
+                <h1 className="text-primary-black text-2xl text-center mb-7 font-bold">Login</h1>
+                <LoginForm onSubmit={handleFormSubmit} />
+                <Divider />
+                <LoginOptions />
             </motion.div>
         )
 }
